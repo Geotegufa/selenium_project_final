798b2d2324360781ddcc3c4c0ee608cc3d8d8ef5
Geoteg <geotegufa@gmail.com>
2026-02-05 14:51:02 +0500

    Update MainPage with go_to_login_page method and comments

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,17 +1,8 @@
-# Импортируем базовый класс BasePage, от которого будем наследоваться
-from .base_page import BasePage
+from .pages.main_page import MainPage
 
-# Импортируем By, чтобы искать элементы на странице через CSS, ID и т.д.
-from selenium.webdriver.common.by import By
 
-# Создаём класс MainPage, который наследует BasePage
-# Наследование означает, что MainPage получает все методы и атрибуты BasePage
-class MainPage(BasePage):
-
-    # Метод для перехода на страницу логина
-    # self — это ссылка на текущий объект MainPage, через неё обращаемся к браузеру и другим методам
-    def go_to_login_page(self):
-        # Находим ссылку на логин по CSS-селектору
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        # Кликаем по найденной ссылке, чтобы перейти на страницу логина
-        login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
